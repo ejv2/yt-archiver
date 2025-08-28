484385429888a0a733522059018be2c5cc228653
Ethan Marshall <ethan@ejv2.cc>
2025-08-28 01:29:09 +0100

    Add basic web interface

---
+++ cmd/ytarchiver-web/main.go
@@ -0,0 +1,274 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	ListenAddr = flag.String("listen", ":80", "Address to listen on, in the format [hostname]:port")
+	Root       = flag.String("root", ".", "ytarchiver root directory to load files from")
+)
+
+type channelData struct {
+	ID   string
+	Name string
+}
+
+type videoData struct {
+	ID           string `json:"id"`
+	Title        string `json:"title"`
+	Description  string `json:"description"`
+	ThumbnailURL string `json:"thumbnail"`
+	Duration     string `json:"duration_string"`
+	ChannelID    string `json:"channel_id"`
+	Timestamp    uint   `json:"timestamp"`
+	WasLive      bool   `json:"was_live"`
+	Extension    string `json:"ext"`
+}
+
+type videoArray []videoData
+
+func (v videoArray) Len() int {
+	return len(v)
+}
+
+func (v videoArray) Less(i, j int) bool {
+	// NOTE: Sorting in reverse here so that most recent timestamp comes first.
+	return v[i].Timestamp > v[j].Timestamp
+}
+
+func (v videoArray) Swap(i, j int) {
+	tmp := v[i]
+	v[i] = v[j]
+	v[j] = tmp
+}
+
+type standardData struct {
+	Chans  []channelData
+	Videos map[string]videoArray
+}
+
+func loadStandardData() (standardData, error) {
+	dat := standardData{Videos: make(map[string]videoArray)}
+
+	chandirs, err := os.ReadDir(*Root)
+	if err != nil {
+		return dat, fmt.Errorf("standard data: reading channels: %w", err)
+	}
+
+	for _, c := range chandirs {
+		if !c.IsDir() {
+			continue
+		}
+
+		path := filepath.Join(*Root, c.Name(), "channel.json")
+		fdat, err := os.ReadFile(path)
+		if err != nil {
+			return dat, fmt.Errorf("standard data: reading channel data: %w", err)
+		}
+
+		var chanobj channelData
+		err = json.Unmarshal(fdat, &chanobj)
+		if err != nil {
+			return dat, fmt.Errorf("standard data: parsing channel data: %w", err)
+		}
+
+		dat.Chans = append(dat.Chans, chanobj)
+
+		chanpath := filepath.Join(*Root, c.Name())
+		vidfiles, err := os.ReadDir(chanpath)
+		if err != nil {
+			return dat, fmt.Errorf("standard data: reading channel videos: %w", err)
+		}
+
+		for _, v := range vidfiles {
+			if strings.HasSuffix(v.Name(), ".info.json") {
+				path := filepath.Join(*Root, c.Name(), v.Name())
+				fdat, err := os.ReadFile(path)
+				if err != nil {
+					return dat, fmt.Errorf("standard data: reading video data: %w", err)
+				}
+
+				var video videoData
+				err = json.Unmarshal(fdat, &video)
+				if err != nil {
+					return dat, fmt.Errorf("standard data: parsing video data: %w", err)
+				}
+
+				dat.Videos[chanobj.ID] = append(dat.Videos[chanobj.ID], video)
+			}
+		}
+
+		// Sort in descending order of unix timestamp (i.e most recent first)
+		sort.Sort(dat.Videos[chanobj.ID])
+	}
+
+	return dat, nil
+}
+
+// loadStandardDataChannel is kind of lazy and inefficient, but what the hell
+func loadStandardDataChannel(cid string) (standardData, int, error) {
+	dat, err := loadStandardData()
+	if err != nil {
+		return dat, -1, err
+	}
+
+	chanind := 0
+	for i, c := range dat.Chans {
+		if c.ID == cid {
+			chanind = i
+			break
+		}
+	}
+
+	return dat, chanind, nil
+}
+
+// loadStandardDataVideo has the same problems (if not worse) as loadStandardDataChannel,
+// but I am tired and can't be bothered.
+func loadStandardDataVideo(cid, vid string) (standardData, int, int, error) {
+	dat, chanind, err := loadStandardDataChannel(cid)
+	if err != nil {
+		return dat, -1, -1, err
+	}
+
+	vind := -1
+	for i, v := range dat.Videos[cid] {
+		if v.ID == vid {
+			vind = i
+			break
+		}
+	}
+
+	return dat, chanind, vind, nil
+}
+
+func limitString(arg string, lim int) string {
+	if len(arg) < lim {
+		return arg
+	}
+
+	return arg[:lim] + "..."
+}
+
+func handleRoot(c *gin.Context) {
+	dat, err := loadStandardData()
+	if err != nil {
+		c.AbortWithError(500, err)
+	}
+
+	c.HTML(200, "index.gohtml", dat)
+}
+
+func handleChannel(c *gin.Context) {
+	cid := c.Param("id")
+	if cid == "" {
+		log.Panicln("got empty ID parameter in required route")
+	}
+
+	dat, cind, err := loadStandardDataChannel(cid)
+	if err != nil {
+		c.AbortWithError(500, err)
+	}
+
+	c.HTML(200, "channel.gohtml", struct {
+		standardData
+		Cid  string
+		Cind int
+	}{dat, cid, cind})
+}
+
+func handleVideo(c *gin.Context) {
+	cid := c.Param("cid")
+	vid := c.Param("id")
+	if cid == "" || vid == "" {
+		log.Panicln("got empty ID/VID parameter in required route")
+	}
+
+	dat, cind, vind, err := loadStandardDataVideo(cid, vid)
+	if err != nil {
+		c.AbortWithError(500, err)
+	}
+
+	c.HTML(200, "video.gohtml", struct {
+		standardData
+		Cid  string
+		Vid  string
+		Cind int
+		Vind int
+	}{dat, cid, vid, cind, vind})
+}
+
+func handleHelp(c *gin.Context) {
+	dat, err := loadStandardData()
+	if err != nil {
+		c.AbortWithError(500, err)
+	}
+
+	c.HTML(200, "help.gohtml", dat)
+}
+
+func main() {
+	log.Println("Starting ytarchiver web interface...")
+	flag.Parse()
+
+	// Startup and listen
+	router := gin.New()
+	srv := http.Server{
+		Addr:              *ListenAddr,
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       15 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+	router.Use(gin.Logger(), gin.Recovery())
+	router.FuncMap["limit"] = limitString
+	router.LoadHTMLGlob("*.gohtml")
+
+	router.GET("/", handleRoot)
+	router.GET("/chan/:id", handleChannel)
+	router.GET("/vid/:cid/:id", handleVideo)
+	router.GET("/help", handleHelp)
+	router.Static("/videos/", *Root)
+
+	errchan := make(chan error, 1)
+	sigchan := make(chan os.Signal, 1)
+
+	signal.Notify(sigchan, os.Interrupt)
+	go func() {
+		err := srv.ListenAndServe()
+		errchan <- err
+	}()
+
+	select {
+	case <-sigchan:
+		log.Println("Caught interrupt signal. Terminating gracefully...")
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			if err == ctx.Err() {
+				log.Println("Shutdown timeout reached. Terminating forcefully...")
+				return
+			}
+			log.Fatal(err)
+		}
+	case err := <-errchan:
+		if err != http.ErrServerClosed {
+			log.Panic(err) // NOTREACHED: unless fatal error
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.24.6
 
 require (
 	github.com/cristalhq/aconfig v0.19.0
-	github.com/ejv2/prepper v0.0.0-20250820001341-533664d19899
+	github.com/gin-gonic/gin v1.9.1
 	google.golang.org/api v0.248.0
 )
 
@@ -12,19 +12,38 @@ require (
 	cloud.google.com/go/auth v0.16.5 // indirect
 	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
 	cloud.google.com/go/compute/metadata v0.8.0 // indirect
+	github.com/bytedance/sonic v1.10.1 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.15.4 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/s2a-go v0.1.9 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
 	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
 	go.opentelemetry.io/otel v1.36.0 // indirect
 	go.opentelemetry.io/otel/metric v1.36.0 // indirect
 	go.opentelemetry.io/otel/trace v1.36.0 // indirect
+	golang.org/x/arch v0.5.0 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
@@ -33,4 +52,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250818200422-3122310a409c // indirect
 	google.golang.org/grpc v1.74.2 // indirect
 	google.golang.org/protobuf v1.36.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
